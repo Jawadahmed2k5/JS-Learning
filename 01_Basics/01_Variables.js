472fa3b510e8cd62768beef6e58d10616a46bad3
Jawad Ahmed <officialjawad25@gmail.com>
2025-04-24 05:42:11 +0000

    conversion of data types

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "jawadahmedtanoli@gmail.com"
+var accountPassword = "12345"
+accountCity = "Abbottabad"
+let accountState;
+
+// accountId //not allowed
+accountEmail = "jaja@gi.com"
+accountPassword = "112233"
+accountCity = "up"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer Not to use "Var"
+/*
